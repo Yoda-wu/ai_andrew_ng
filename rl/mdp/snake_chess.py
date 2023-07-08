506b9e532ebeb376d8ade7d22bf62e4e79e70e7e
Marchons <d1286360646@outlook.com>
2023-07-08 22:56:48 +0800

    add snake_chess

--- rl/mdp/snake_chess.py
+++ rl/mdp/snake_chess.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import numpy as np
 import gym
 from gym.spaces import Discrete, Box
@@ -23,4 +24,58 @@ class Snake_Env(gym.Env):
 
         if ladder_num == 0:
             self.ladders = {0 : 0}
+        else:
+            # 处理梯子的值，梯子的值不能超过格子数，让梯子无重复反向赋值
+            ladders =  set(np.random.randint(1, self.SIZE, size=ladder_num))
+            while len(ladders) < self.ladder_num * 2:
+                ladders.add(np.random.randint(1, self.SIZE))
+
+            ladders = list(ladders)
+            ladders = np.array(ladders)
+            np.random.shuffle(ladders)
+            ladders = ladders.reshape((self.ladder_num, 2))
+
+            re_ladders = list()
+            for i in ladders:
+                re_ladders.append([i[1],i[0]])
+            
+            re_ladders = np.array(re_ladders)
+            self.ladders = dict(np.append(re_ladders, ladders, axis=0))
+
+        print(f"ladders: {self.ladders}, dice: {self.dices}")
+        self.pos = 1
+    
+    def reset(self):
+        self.pos = 1
+        return self.pos
+
+    def step(self, a):
+        step = np.random.randint(1, self.dices[a]+1)
+        self.pos += step
+        if self.pos == 100:
+            return 100 , 100 , 1, {} # pos， reward， done， info
+        elif self.pos > 100:
+            self.pos = 200 - self.pos
         
+        if self.pos in self.ladders:
+            self.pos = self.ladders[self.pos]
+        
+        return self.pos, -1, 0, {} # pos， reward， done， info
+    
+
+    def reward(self, s):
+        if s == 100 :
+            return 100
+        return -1
+    
+    def render(self):
+        pass
+
+
+
+def Table_Agent(object):
+    def __init__(self, env):
+        self.s_len = env.observation_space.n
+        self.a_len = env.action_space.n
+
+ 

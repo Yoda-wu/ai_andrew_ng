bcfb5b952b0c61233fb15f5417024c0602445caf
Marchons <d1286360646@outlook.com>
2023-08-29 14:08:20 +0800

    add reinforce

--- rl/reinforce/reinforce.py
+++ rl/reinforce/reinforce.py
@@ -21,5 +21,36 @@ class PolicyNet(torch.nn.Module):
     
 
 class ReinForce:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, state_dim, hidden_dim, action_dim, learning_rate, gamma,
+                 device) -> None:
+        self.policy_net = PolicyNet(state_dim,hidden_dim,action_dim)
+        self.optimizer = torch.optim.Adam(self.policy_net.parameters(), lr = learning_rate)
+        self.gamma = gamma
+
+        self.device = device
+
+
+    def take_action(self, state):
+        state = torch.tensor([state],   dtype=torch.float).to(self.device)
+        probs = self.policy_net(state)
+        action_dist = torch.distributions.Categorical(probs)
+        action = action_dist.sample()
+        return action.item() 
+    
+    def update(self, transition_dict):
+        reward_list = transition_dict['rewards']
+        state_list = transition_dict['states']
+        action_list = transition_dict['actions']
+
+        G = 0
+        self.optimizer.zero_grad()
+
+        for i in reversed(range(len(reward_list))):
+            reward = reward_list[i]
+            state = torch.tensor([state_list[i]], dtype=torch.float).to(self.device)
+            action = torch.tensor([action_list[i]]).view(-1, 1).to(self.device)
+            log_prob = torch.log(self.policy_net(state).gather(1, action))
+            G = self.gamma * G + reward
+            loss = -log_prob * G  # 每一步的损失函数
+            loss.backward()  # 反向传播计算梯度
+        self.optimizer.step()  # 梯度下降
